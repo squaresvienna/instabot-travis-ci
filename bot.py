841b394a95146caa86594dee6c320aece8dd1abd
squaresvienna <69768476+squaresvienna@users.noreply.github.com>
2020-08-18 08:41:54 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -22,28 +22,31 @@ bot = Bot(
             max_unfollows_per_day=20,
             max_comments_per_day=50,
             max_likes_to_like=100,
-            filter_users=True,
+            filter_users=False,
+            filter_verified_accounts=True,
             max_followers_to_follow=30000,
             min_followers_to_follow=10,
             max_following_to_follow=7500,
             min_following_to_follow=10,
             max_following_to_block=2000,
             min_media_count_to_follow=3,
+            max_likes_to_like=300,
             like_delay=20,
             unlike_delay=20,
             follow_delay=30,
             unfollow_delay=30,
             comment_delay=50,
             comments_file="comments.txt",
+            max_likes_to_like=./,
 )
 bot.login(
     username=os.getenv("INSTAGRAM_USERNAME"), 
     password=os.getenv("INSTAGRAM_PASSWORD"),
 )
 
-bot.like_hashtag("synthwave")
+bot.like_hashtag("cyberpunk")
 
-bot.comment_hashtag("synthwave")
+bot.comment_hashtag("cyberpunk")
 
 bot.follow("activefollower")
 
